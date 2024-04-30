35d8f741cd651e63d5ab9ec30def263e09784778
418Coffee <matthijskes@gmail.com>
2024-04-30 21:25:49 +0200

    feat: add quote filter

--- tools/plugin.py
+++ tools/plugin.py
@@ -9,9 +9,12 @@ def to_yaml(value: Any) -> str:
         return yaml.dump(value, default_flow_style=False, indent=0)
     return str(value).lower() # hacky
 
+def quote(value: Any) -> str:
+    return f'"{str(value)}"'
+
 class Plugin(makejinja.plugin.Plugin):
     def filters(self) -> makejinja.plugin.Filters:
-        return [to_yaml]
+        return [to_yaml, quote]
 
     def functions(self) -> makejinja.plugin.Functions:
         return []
